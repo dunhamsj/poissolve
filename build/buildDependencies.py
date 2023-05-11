6752bae76c2e59bd7f28c15de2b98a191bb7542f
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-05-11 13:21:41 -0400

    added buildDependencies.py file

---
+++ build/buildDependencies.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+"""
+Build Makefile_Dependencies* files for source code
+"""
+
+# TODO: Read in POISSOLVE_ROOT and VPATH from environment
+
+import os
+
+POISSOLVE_ROOT = '/home/kkadoogan/Work/Codes/poissolve/'
+VPATH = [ POISSOLVE_ROOT + 'modules' ]
+
+def getDeps( file ):
+    deps = []
+    with open( file, 'r' ) as f2:
+        for line in f2:
+            if 'IMPLICIT NONE' in line: break
+            if '  USE' in line: deps.append( line[6:].split( ',' )[0] + '.o' )
+    return deps
+
+owd = os.getcwd()
+
+for iPath in range( len( VPATH ) ):
+
+    nwd = VPATH[iPath]
+
+    os.chdir( nwd )
+
+    files = os.listdir( nwd )
+
+    f90 = {}
+    for iFile in range( len( files ) ):
+        if files[iFile][-3:] == 'f90':
+            deps = getDeps( nwd + '/' + files[iFile] )
+            f90[files[iFile]] = deps
+
+    keys   = list( f90.keys() )
+    values = list( f90.values() )
+
+    makefile = 'Makefile_Dependencies_{:}' \
+               .format( VPATH[iPath].split( '/' )[-1] )
+
+    os.system( 'rm -f Makefile_Dependencies_{:}'.format( makefile ) )
+
+    with open( makefile, 'w' ) as f:
+        for i in range( len( keys ) ):
+            f.write( '{:}.o: \\\n'.format( keys[0][:-4] ) )
+            if len( values[i] ) > 0:
+                for j in range( len( values[i] ) ):
+                    f.write( '  {:} \\\n'.format( values[i][j] ) )
+            f.write( '  {:}\n'.format( keys[i] ) )
+            if not i == len( keys ) - 1:
+                f.write( '\n' )
