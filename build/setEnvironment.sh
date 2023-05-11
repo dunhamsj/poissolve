503bfcf9b2ce98b53032f027af065d78a3157e00
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-05-11 13:18:09 -0400

    added machine kkadoogan

--- build/setEnvironment.sh
+++ build/setEnvironment.sh
@@ -2,12 +2,19 @@
 
 export POISSOLVE_MACHINE=$1
 
-if [[ ${POISSOLVE_MACHINE} == dunhamsj ]]; then
+if   [[ ${POISSOLVE_MACHINE} == dunhamsj ]]; then
 
     echo ""
     echo "  INFO: Setting poissolve environment for" ${POISSOLVE_MACHINE}
     echo ""
     export POISSOLVE_ROOT=/Users/dunhamsj/Work/Codes/poissolve
+
+elif [[ ${POISSOLVE_MACHINE} == kkadoogan ]]; then
+
+    echo ""
+    echo "  INFO: Setting poissolve environment for" ${POISSOLVE_MACHINE}
+    echo ""
+    export POISSOLVE_ROOT=/home/kkadoogan/Work/Codes/poissolve
 
 else
 
