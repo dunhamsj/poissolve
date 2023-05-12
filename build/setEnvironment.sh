d9cbfb5bbceaf81a6162a1e81f5769812f9c1ce0
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-05-12 13:23:50 -0400

    changes to kkadoogan and dunhamsj environments

--- build/setEnvironment.sh
+++ build/setEnvironment.sh
@@ -7,9 +7,8 @@ if   [[ ${POISSOLVE_MACHINE} == dunhamsj ]]; then
     echo ""
     echo "  INFO: Setting poissolve environment for" ${POISSOLVE_MACHINE}
     echo ""
-    export POISSOLVE_ROOT=/Users/dunhamsj/Work/Codes/poissolve
-    export CC=/usr/local/Cellar/mpich/4.1.1_1/bin/mpicc
-    export FC=/usr/local/Cellar/mpich/4.1.1_1/bin/mpif90
+    export CC=mpicc
+    export FC=mpif90
     export FFLAGS=-g
 
 elif [[ ${POISSOLVE_MACHINE} == kkadoogan ]]; then
@@ -17,9 +16,9 @@ elif [[ ${POISSOLVE_MACHINE} == kkadoogan ]]; then
     echo ""
     echo "  INFO: Setting poissolve environment for" ${POISSOLVE_MACHINE}
     echo ""
-    export POISSOLVE_ROOT=/home/kkadoogan/Work/Codes/poissolve
     export CC=mpicc
     export FC=mpif90
+    export FFLAGS=-g
 
 else
 
