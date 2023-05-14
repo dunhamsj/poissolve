fb472dee9124b911239f5160de5e8bf5cdb42799
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-05-14 10:57:47 -0400

    Initialized iX_B0 and iX_E0

--- Applications/TestInitialization/TestInitialization.f90
+++ Applications/TestInitialization/TestInitialization.f90
@@ -12,6 +12,9 @@ PROGRAM TestInitialization
 
   INTEGER :: nMF = 13
 
+  iX_B0 = [ 1, 1, 1 ]
+  iX_E0 = [ 10, 1, 1 ]
+
   ALLOCATE( f(iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),1:nMF) )
 
   CALL InitializePoissolve( iX_B0, iX_E0, f )
