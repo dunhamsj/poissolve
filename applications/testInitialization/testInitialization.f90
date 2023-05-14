91af219d034ddddf1500a687539bd8391146ee55
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-05-13 23:29:53 -0400

    parallel make working

--- applications/testInitialization/testInitialization.f90
+++ applications/testInitialization/testInitialization.f90
@@ -7,6 +7,15 @@ PROGRAM TestInitialization
 
   IMPLICIT NONE
 
-  REAL(DP) :: x
+  INTEGER :: iX_B0(3), iX_E0(3)
+  REAL(DP), ALLOCATABLE :: f(:,:,:,:)
+
+  INTEGER :: nMF = 13
+
+  ALLOCATE( f(iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),1:nMF) )
+
+  CALL InitializePoissolve( iX_B0, iX_E0, f )
+
+  DEALLOCATE( f )
 
 END PROGRAM TestInitialization
--- build/buildDependencies.py
+++ build/buildDependencies.py
@@ -6,7 +6,11 @@ Build Makefile_Dependencies* files for source code
 
 import os
 
-def getDeps_file( file ):
+def getDeps_fortran( file ):
+
+    """
+    Get dependencies for fortran file `file`
+    """
 
     deps = []
 
@@ -17,149 +21,169 @@ def getDeps_file( file ):
             if (  'IMPLICIT NONE' in line \
                or 'implicit none' in line ): break
 
-            if ( 'USE ' in line ):
-                stripped = line.split( 'USE' )[1][1:].split( ',' )[0] + '.o'
-                deps.append( stripped )
+            if len( line ) <= 1: continue
 
-            if ( 'use ' in line ):
-                stripped = line.split( 'use' )[1][1:].split( ',' )[0] + '.o'
-                deps.append( stripped )
+            line_split = line.split( ' ' )
+            line_split_nospaces = [ s for s in line_split if s != '' ]
+
+            if len( line_split_nospaces ) <= 1: continue
+
+            if( line_split_nospaces[0] == 'MODULE' ): continue
+            if( line_split_nospaces[0] == 'module' ): continue
+
+            if line_split_nospaces[1][-1] == ',':
+                if line_split_nospaces[1][-1] == '\n':
+                    deps.append( line_split_nospaces[1][:-2] )
+                else:
+                    deps.append( line_split_nospaces[1][:-1] )
+            else:
+                if line_split_nospaces[1][-1] == '\n':
+                    deps.append( line_split_nospaces[1][:-1]   )
+                else:
+                    deps.append( line_split_nospaces[1][:]   )
 
     return deps
 
-def getDeps( VPATH ):
+def getDeps( VPATH, suffix ):
 
-    f90 = {}
+    deps = {}
 
     for iPath in range( len( VPATH ) ):
 
-        nwd = VPATH[iPath]
+        vpath = VPATH[iPath]
 
-        files = os.listdir( nwd )
+        files = os.listdir( vpath )
 
         for iFile in range( len( files ) ):
 
-            if files[iFile][-3:] == 'f90':
+            if ( files[iFile][-len(suffix):] == suffix ):
 
-                f90[files[iFile]] = getDeps_file( nwd + '/' + files[iFile] )
-    return f90
+                deps[files[iFile]] \
+                  = getDeps_fortran( vpath + '/' + files[iFile] )
 
-VPATH = os.getenv( 'VPATH' ).split( ' ' )
-#VPATH \
-#  = [ '/Users/dunhamsj/Work/Codes/poissolve/applications/testInitialization', \
-#      '/Users/dunhamsj/Work/Codes/poissolve/modules/Initialization', \
-#      '/Users/dunhamsj/Work/Codes/poissolve/modules/Library' ]
+    return deps
 
-f90 = getDeps( VPATH )
+def makeDictionary( VPATH, suffix ):
 
-keys   = list( f90.keys() )
-values = list( f90.values() )
+    deps = getDeps( VPATH, suffix )
+
+    srcs = list( deps.keys  () )
+    objs = list( deps.values() )
+
+    # Sort dictionary by dependencies: least dependencies first
+
+    ind = [ i for i in range( len( srcs ) ) ]
+
+    for iSrc in range( len( srcs ) ):
+
+        for jSrc in range( len( srcs ) ):
+
+            if iSrc == jSrc: continue
+
+            for iObj in range( len( objs[iSrc] ) ):
+
+                if( objs[iSrc][iObj][:-2] == srcs[jSrc][:-(len(suffix)+1)] ):
+
+                    a, b = ind.index(iSrc), ind.index(jSrc)
+
+                    ind[b], ind[a] = ind[a], ind[b]
+
+                    break
+
+    ind = [ i for i in ind[::-1] ]
+
+    deps_new = {}
+
+    for iSrc in range( len( srcs ) ):
+
+        deps_new[srcs[ind[iSrc]]] = objs[ind[iSrc]]
+
+    return deps_new
+
+def copyFiles( deps, VPATH, nwd, suffix ):
+
+    srcs = list( deps.keys  () )
+    objs = list( deps.values() )
+
+    for iPath in range( len( VPATH ) ):
+
+        vpath = VPATH[iPath]
+
+        for iSrc in range( len( srcs ) ):
+
+            oldSrc = '{:}/{:}'.format( vpath, srcs[iSrc] )
+
+            if not os.path.isfile( oldSrc ): continue
+
+            newSrc = '{:}/{:}'.format( nwd, suffix + '_' + srcs[iSrc] )
+
+            os.system( 'cp {:} {:}'.format( oldSrc, newSrc ) )
+
+    return
+
+def WriteMakefile_ObjectFiles( deps, fileName ):
+
+    srcs = list( deps.keys() )
+
+    with open( fileName, 'w' ) as f:
+
+        f.write( 'POISSOLVE_OBJ = \\\n' )
+
+        for iSrc in range( len( srcs ) ):
+
+            if iSrc < len( srcs ) - 1:
+
+                f.write( '  {:} \\\n' \
+                         .format( srcs[iSrc].replace( '.f90', '.o' ) ) )
+            else:
+
+                f.write( '  {:}\n' \
+                         .format( srcs[iSrc].replace( '.f90', '.o' ) ) )
+
+def WriteMakefile_Dependencies( deps, fileName ):
+
+    srcs = list( deps.keys  () )
+    objs = list( deps.values() )
+
+    with open( fileName, 'w' ) as f:
+
+        for iSrc in range( len( srcs ) ):
+
+            f.write( '{:}: \\\n'.format( srcs[iSrc].replace( '.f90', '.o' ) ) )
+
+            for iObj in range( len( objs[iSrc] ) ):
+
+                f.write( '  {:}.o \\\n'.format( objs[iSrc][iObj] ) )
+
+            f.write( '  {:}\n'.format( srcs[iSrc] ) )
+
+            if iSrc < len( srcs ) - 1: f.write( '\n' )
 
 POISSOLVE_ROOT = os.getenv( 'POISSOLVE_ROOT' )
 
-owd = os.getcwd()
-#owd = POISSOLVE_ROOT + '/applications/testInitialization'
+VPATH  = os.getenv( 'VPATH' ).split( ' ' )
+VPATH2 = os.getenv( 'VPATH2' ).split( ' ' )
+BLDDIR     = os.getenv( 'BLDDIR' )
+#VPATH = [ '/home/kkadoogan/Work/Codes/poissolve/modules/Initialization', \
+#          '/home/kkadoogan/Work/Codes/poissolve/modules/Library', \
+#          '/home/kkadoogan/Work/Codes/poissolve/applications/testInitialization' ]
 
-objFiles = []
+nwd = BLDDIR + '/src'
 
-for iPath in range( len( VPATH ) ):
+deps_f90 = makeDictionary( VPATH, 'f90' )
 
-    nwd = VPATH[iPath]
+copyFiles( deps_f90, VPATH, nwd, 'f90' )
+WriteMakefile_ObjectFiles \
+  ( deps_f90, \
+    '{:}/build/Makefile_Poissolve_ObjectFiles'.format( POISSOLVE_ROOT ) )
+    #'{:}/../../build/Makefile_Poissolve_ObjectFiles'.format( BLDDIR ) )
+WriteMakefile_Dependencies \
+  ( deps_f90, \
+    '{:}/build/Makefile_Poissolve_Dependencies'.format( POISSOLVE_ROOT ) )
+    #'{:}/../../build/Makefile_Poissolve_Dependencies'.format( BLDDIR ) )
 
-    for iKey in range( len( keys ) ):
-
-        oldSrc = '{:}/{:}'.format( nwd, keys[iKey] )
-
-        if not os.path.isfile( oldSrc ): continue
-
-        newSrc = '{:}/src/{:}'.format( owd, keys[iKey] )
-
-        os.system( 'cp {:} {:}'.format( oldSrc, newSrc ) )
-
-        objFiles.append( '{:}'.format( keys[iKey].replace( '.f90', '.o' ) ) )
-
-        if len( values[iKey] ) > 0:
-
-            for iValue in range( len( values[iKey] ) ):
-
-                objFiles.append( values[iKey][iValue] )
-
-# Sort dictionary by dependencies
-
-ind = [ i for i in range( len( keys ) ) ]
-
-for iKey in range( len( keys ) ):
-
-    for jKey in range( len( keys ) ):
-
-        if iKey == jKey: continue
-
-        for iValue in range( len( values[iKey] ) ):
-
-            if values[iKey][iValue][:-2] == keys[jKey][:-4]:
-
-                a, b = ind.index(iKey), ind.index(jKey)
-
-                ind[b], ind[a] = ind[a], ind[b]
-
-                break
-
-f90 = {}
-
-for iKey in range( len( keys ) ):
-
-    f90[keys[ind[iKey]]] = values[ind[iKey]]
-
-keys   = list( f90.keys() )
-values = list( f90.values() )
-
-# Generate Makefile_Poissolve_ObjectFiles
-
-with open( POISSOLVE_ROOT \
-             + '/build/Makefile_Poissolve_ObjectFiles', 'w' ) as f:
-
-    f.write( 'POISSOLVE_OBJ = \\\n' )
-
-    for iKey in range( len( keys ) ):
-
-        if iKey < len( keys ) - 1:
-
-            f.write( '  {:}{:} \\\n' \
-                     .format( owd + '/obj/', \
-                              keys[iKey].replace( '.f90', '.o' ) ) )
-
-        else:
-
-            f.write( '  {:}{:} \n' \
-                     .format( owd + '/obj/', \
-                              keys[iKey].replace( '.f90', '.o' ) ) )
-
-# Generate Makefile_Poissolve_Dependencies
-
-with open( POISSOLVE_ROOT \
-             + '/build/Makefile_Poissolve_Dependencies', 'w' ) as f:
-    for iKey in range( len( keys ) ):
-
-        f.write( '{:}: \\\n'.format( keys[iKey].replace( '.f90', '.o' ) ) )
-
-        for iValue in range( len( values[iKey] ) ):
-
-            f.write( '  {:} \\\n'.format( values[iKey][iValue] ) )
-
-        f.write( '  {:}\n'.format( keys[iKey] ) )
-
-        if iKey < len( keys ) - 1: f.write( '\n' )
-
-# Create list of object files and source files
-
-with open( owd + '/obj/objFiles', 'w' ) as f:
-
-    for iKey in range( len( keys ) ):
-
-        f.write( keys[iKey].replace( '.f90','.o' ) + '\n' )
-
-with open( owd + '/src/srcFiles', 'w' ) as f:
-
-    for iKey in range( len( keys ) ):
-
-        f.write( keys[iKey] + '\n' )
+#deps_f902 = makeDictionary( VPATH2, 'f90' )
+#
+#WriteMakefile_Dependencies \
+#  ( deps_f902, \
+#    '{:}/Makefile_Driver'.format( BLDDIR ) )
